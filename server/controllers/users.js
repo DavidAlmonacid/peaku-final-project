998426e2bdb135c79fd5874d8d2fccc1b01c6e56
David Almonacid <david.almonacidf@gmail.com>
2024-01-16 13:39:35 -0500

    Refactor user registration endpoint

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,26 @@
+import bcrypt from 'bcrypt';
+import { validateUser } from '../schemas/user.js';
+
+const saltRounds = 12;
+
 export class UserController {
-  static createUser = async (req, res) => {
-    const { body } = req;
-    console.log(body);
-    return res.json({ body });
+  static register = async (req, res) => {
+    const validationResult = validateUser(req.body);
+
+    if (validationResult.error) {
+      const message = validationResult.error.issues[0].message;
+      return res.status(400).json({ error: message });
+    }
+
+    let data = validationResult.data;
+
+    try {
+      const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+      data = { ...data, password: hashedPassword };
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    return res.send(validationResult);
   };
 }
