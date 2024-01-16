ac2eefc9ad1a1851dac550a6e90b5202a87551f9
David Almonacid <david.almonacidf@gmail.com>
2024-01-16 11:55:43 -0500

    Rename file

--- database/tables.sql
+++ database/tables.sql
@@ -1,10 +1,9 @@
--- PostgreSQL 16.1
-
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username VARCHAR(40) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
