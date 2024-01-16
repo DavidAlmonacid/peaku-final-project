2cfb38dbf5aee3bb72f1630706be8165f98254ed
David Almonacid <david.almonacidf@gmail.com>
2024-01-16 13:33:47 -0500

    Add user schema and validation functions

---
+++ server/schemas/user.js
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const userSchema = z.object({
+  name: z.string(),
+  last_name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(8)
+});
+
+export const validateUser = (object) => {
+  return userSchema.safeParse(object);
+};
+
+export const validatePartialUser = (object) => {
+  return userSchema.partial().safeParse(object);
+};
