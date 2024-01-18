aff6570dbf910662e1dedc33d4e799daab6c3f99
David Almonacid <david.almonacidf@gmail.com>
2024-01-17 23:39:53 -0500

    Add TailwindCSS

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Red Hat Display", "sans-serif"],
+      title: ["Readex Pro", "sans-serif"]
+    },
+    extend: {}
+  },
+  plugins: []
+};
