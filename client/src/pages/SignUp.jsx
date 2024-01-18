4096d840fa12fdda50912ee0bb4b7787fc3876b3
David Almonacid <david.almonacidf@gmail.com>
2024-01-18 14:56:42 -0500

    Add signup image and update form styling in SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,5 @@
+import signupImage from "../assets/signup-image.webp";
+
 export default function SignUp() {
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -16,27 +18,56 @@ export default function SignUp() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div
-        className="flex flex-col gap-7 max-w-md *:bg-accent-antiflash-white *:text-accent-night *:h-9 *:px-4 *:rounded-xl *:focus-within:outline-primary-royal-blue"
-        style={{ colorScheme: "light" }}
-      >
-        <input type="text" name="name" placeholder="Name *" />
-        <input type="text" name="last_name" placeholder="Last name *" />
-        <input
-          type="email"
-          name="email"
-          pattern="\w+@\w+\.\w{2,3}"
-          placeholder="Email address *"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter your password *"
-        />
-      </div>
+    <div className="flex justify-center items-center h-full">
+      <section className="flex flex-col items-center gap-y-14 px-6 py-12 bg-accent-antiflash-white/20 backdrop-blur-sm w-full max-w-lg mx-3 rounded-3xl">
+        <h1 className="text-4xl font-semibold">Create an account</h1>
 
-      <input type="submit" value="Sign Up" />
-    </form>
+        <form className="w-full" onSubmit={handleSubmit}>
+          <div
+            className="flex flex-col gap-y-6 *:bg-accent-antiflash-white *:text-accent-night *:w-full *:h-9 *:px-4 *:rounded-xl *:focus-within:outline-primary-royal-blue"
+            style={{ colorScheme: "light" }}
+          >
+            <input type="text" name="name" placeholder="Name *" required />
+            <input
+              type="text"
+              name="last_name"
+              placeholder="Last name *"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              pattern="\w+@\w+\.\w{2,3}"
+              placeholder="Email address *"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password *"
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mt-11 bg-primary-royal-blue text-accent-antiflash-white text-lg font-medium h-10 w-full rounded-xl"
+          >
+            Create Account
+          </button>
+        </form>
+
+        <p className="flex gap-x-3 text-sm">
+          <span>Already have an account?</span>
+          <button>Login</button>
+        </p>
+      </section>
+
+      <div className="absolute right-0 bottom-0 -z-10">
+        <picture className="block max-w-64 p-3 md:p-4 lg:p-5 box-content">
+          <img src={signupImage} alt="fantasy character" />
+        </picture>
+      </div>
+    </div>
   );
 }
