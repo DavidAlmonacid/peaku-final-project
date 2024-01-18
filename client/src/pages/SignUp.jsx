a0e3499f5be4ebe5d99b11e8314d3560c88d29b0
David Almonacid <david.almonacidf@gmail.com>
2024-01-18 11:14:51 -0500

    Improve styles and fetching the backend endpoint to create a new user

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,26 @@
 export default function SignUp() {
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    console.log(JSON.stringify(data));
+
+    const response = await fetch("http://localhost:3001/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const json = await response.json();
+    console.log(json);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="">
+      <div
+        className="flex flex-col gap-7 max-w-md *:bg-accent-antiflash-white *:text-accent-night *:h-9 *:px-4 *:rounded-xl *:focus-within:outline-primary-royal-blue"
+        style={{ colorScheme: "light" }}
+      >
         <input type="text" name="name" placeholder="Name *" />
         <input type="text" name="last_name" placeholder="Last name *" />
         <input
