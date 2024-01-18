82cb11914744202125d6295692fccf97eb68938b
David Almonacid <david.almonacidf@gmail.com>
2024-01-18 14:55:39 -0500

    Update header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,22 +6,22 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center px-8 py-4">
       <Link to="/" className="[&>picture>img]:max-h-6">
-        <picture className="hidden sm:block">
+        <picture className="hidden xs:block">
           <img src={logoBig} alt="game store logo big" />
         </picture>
 
-        <picture className="sm:hidden">
+        <picture className="xs:hidden">
           <img src={logoShort} alt="game store logo short" />
         </picture>
       </Link>
 
       <div className="flex gap-x-4 *:py-2 *:px-6 *:rounded-xl *:text-sm *:font-medium">
-        <button className="bg-primary-royal-blue">Log In</button>
+        <button className="bg-primary-royal-blue">Login</button>
         <Link
           to="/signup"
           className="ring-2 ring-inset ring-primary-ruddy-blue text-primary-ruddy-blue"
         >
-          Create Account
+          Sign Up
         </Link>
       </div>
     </header>
