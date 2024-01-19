2d9f16fe36febc9097e346f9f3d8f824709a106a
David Almonacid <david.almonacidf@gmail.com>
2024-01-18 20:03:42 -0500

    Add custom message to the password validation

--- server/schemas/user.js
+++ server/schemas/user.js
@@ -4,7 +4,9 @@ const userSchema = z.object({
   name: z.string(),
   last_name: z.string(),
   email: z.string().email(),
-  password: z.string().min(8)
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long"
+  })
 });
 
 export const validateUser = (object) => {
