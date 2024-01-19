1021d311b9e6fcc2c20f84ce60412f843ebf7632
David Almonacid <david.almonacidf@gmail.com>
2024-01-19 16:39:29 -0500

    Add functionality to the login button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logoBig from "../assets/game-store-logo-big.png";
 import logoShort from "../assets/game-store-logo-short.png";
+import Login from "./Login";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <header className="flex justify-between items-center px-6 py-4 shadow-sm">
       <Link to="/" className="[&>picture>img]:max-h-8">
@@ -16,20 +24,22 @@ export default function Header() {
       </Link>
 
       <div className="flex gap-x-4 *:py-2 *:px-6 *:rounded-xl *:text-sm *:font-medium">
-        <Link
-          to="/login"
+        <button
+          onClick={handleClick}
           className="bg-primary-royal-blue text-accent-antiflash-white"
         >
           Login
-        </Link>
+        </button>
 
         <Link
           to="/signup"
-          className="ring-2 ring-inset ring-primary-royal-blue text-primary-royal-blue"
+          className="ring-2 ring-inset ring-primary-royal-blue text-primary-royal-blue bg-primary-ruddy-blue/10"
         >
           Sign Up
         </Link>
       </div>
+
+      {isOpen && <Login />}
     </header>
   );
 }
