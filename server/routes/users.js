92a3b924b7fd203d7f9e604cb05b4f1d9f1e1004
David Almonacid <david.almonacidf@gmail.com>
2024-01-20 08:02:34 -0500

    Add login route to usersRouter

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,3 +4,4 @@ import { UserController } from "../controllers/users.js";
 export const usersRouter = Router();
 
 usersRouter.post("/", UserController.register);
+usersRouter.get("/", UserController.login);
