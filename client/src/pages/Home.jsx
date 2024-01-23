698ca27d600cdfc985b22556f8e75bd6d8356b8f
David Almonacid <david.almonacidf@gmail.com>
2024-01-23 16:04:20 -0500

    Refactor Home component to use Hero component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import spidermanHero from "../assets/spiderman-hero.png";
+import { Hero } from "../components/Hero.jsx";
 import { BASE_URL } from "../config.js";
 
 async function fetchProducts() {
@@ -34,30 +34,7 @@ export function Home() {
 
   return (
     <>
-      {/* Start Hero */}
-      <div className="px-5 py-12">
-        <div className="flex flex-col xs:flex-row items-end mx-auto max-w-3xl xs:max-h-52 bg-accent-timberwolf rounded-2xl">
-          <section className="flex-auto flex flex-col gap-y-6 p-6 xs:w-1/2">
-            <h2 className="text-3xl font-bold">
-              {promoGame?.name?.split(" - ")[1]}
-            </h2>
-            <p className="line-clamp-5 text-pretty">{promoGame.description}</p>
-            <section>
-              <button>Buy now</button>
-              <button>Add to cart</button>
-            </section>
-          </section>
-
-          <picture className="flex-auto px-6 pt-10 pb-11 xs:w-1/2 xs:h-64">
-            <img
-              src={spidermanHero}
-              alt="Spiderman"
-              className="h-full object-scale-down shadow-black drop-shadow-xl"
-            />
-          </picture>
-        </div>
-      </div>
-      {/* Finish Hero */}
+      <Hero promoGame={promoGame} />
 
       {/* <ul>
         {products.map((product) => (
