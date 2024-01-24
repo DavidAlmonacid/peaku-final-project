81b706bff3143b894d0cb2f76eec5714dc7ba014
David Almonacid <david.almonacidf@gmail.com>
2024-01-24 13:46:40 -0500

    Add CircledArrow component to Icons.jsx

--- client/src/components/Icons.jsx
+++ client/src/components/Icons.jsx
@@ -49,3 +49,20 @@ export const UserIcon = () => (
     />
   </svg>
 );
+
+export const CircledArrow = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2}
+    stroke="currentColor"
+    className="size-5"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>
+);
